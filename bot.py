6af470b4e9a61fe8123e17bb5a0d4062b7cd87df
VaibhavJain1221 <vjain191204@gmail.com>
2025-06-19 14:13:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,8 +28,8 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGODB_URL = os.getenv("MONGODB_URL")
 OWNER_ID = int(os.getenv("OWNER_ID"))
 REQUIRED_CHANNELS = [
-    {"id": "@FreeNetflixDaily0", "name": "Free Prime Videos Daily"},
-    {"id": "@FreeNetflixDailyChat", "name": "Free Prime Videos Daily Chat"}
+    {"id": "", "name": ""},
+    {"id": "", "name": ""}
 ]
 
 pending_referrals = {}  # user_id -> referred_by
